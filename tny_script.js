4ae4068e5de2cd51d181c494ab68b3d044930d58
Justin Aybar <jaybar975@west-mec.org>
2019-02-05 15:25:56 -0700

    1st Commit Push
    Added my first/last name and the date to the HTML and Javascript. Linked the Javascript file to the HTML file and added functions to the file

---
+++ tny_script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Justin Aybar
+   Date: 2.5.19 
+
+*/
+
+/* Display the current date and time */
+document.getElementById("dateNow").innerHTML = "m / d / y < br / > h: m: s";
+
+
+/* Display the time left until New Year's Eve */
+document.getElementById("days").textContent = "dd";
+document.getElementById("hrs").textContent = "hh";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
